020d6a20acd4280fffc7f863a62d4130d0f278e7
Ryan Duncuft <ryanduncuft23@gmail.com>
2025-08-25 08:11:23 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./*.js",
+    // Add other file types/directories if you have them, e.g., "./**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
